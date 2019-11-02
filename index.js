7eebfbadd34ee2c275b1213e915cf9273e6ec235
Ian Walter <public@iankwalter.com>
2019-11-02 00:11:52 -0400

    Cleaning up middleware composition

--- index.js
+++ index.js
@@ -1,26 +1,57 @@
 const {
   validateRegistration,
-  createAccount
+  createAccount,
+  getAccount
 } = require('./middleware/account')
 const { hashPassword } = require('./middleware/password')
+const { generateToken, insertToken } = require('./middleware/token')
 const {
   generateEmailVerificationEmail,
   generatePasswordResetEmail,
   sendEmail
 } = require('./middleware/email')
+const {
+  validateLogin,
+  authenticateLogin,
+  handleAuthentication
+} = require('./middleware/session')
 
 module.exports = {
   validateRegistration,
   hashPassword,
   createAccount,
   generateEmailVerificationEmail,
-  generatePasswordResetEmail,
   sendEmail,
   registration: [
     validateRegistration,
     hashPassword,
     createAccount,
+    generateToken,
+    insertToken,
     generateEmailVerificationEmail,
+    sendEmail
+  ],
+  emailVerfication: [
+    // validateEmailVerification,
+    // getAccountWithEmailTokens
+  ],
+  validateLogin,
+  getAccount,
+  authenticateLogin,
+  handleAuthentication,
+  login: [
+    validateLogin,
+    getAccount,
+    authenticateLogin,
+    handleAuthentication
+  ],
+  generatePasswordResetEmail,
+  passwordReset: [
+    // validatePasswordReset,
+    // getAccountWithPasswordTokens,
+    generateToken,
+    insertToken,
+    generatePasswordResetEmail,
     sendEmail
   ]
 }
