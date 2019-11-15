5f5b607169166c358d15cb27f72605df4b63cd9e
Ian Walter <public@iankwalter.com>
2019-11-15 13:03:51 -0500

    Abstracting more ssr logic to middleware

--- examples/ssr/ssr.js
+++ examples/ssr/ssr.js
@@ -3,18 +3,19 @@ import App from './App'
 export default pageTemplate => ctx => {
   // Render the application using Svelte's SSR API and receive the <head> HTML,
   // body HTML, and CSS code.
-  let page
+  let page = {}
   try {
     page = App.render(ctx)
   } catch (err) {
     // Log the error if one is thrown when rendering the page.
     ctx.log.error(err)
+
+    // Instead of a Svelte app/page, we'll just show the words "Internal Server
+    // Error" on the page.
+    page.html = 'Internal Server Error'
   }
 
-  // Replace the placeholders on the base page template with the code returned
-  // by the render method.
-  return pageTemplate
-    .replace('<% head %>', page.head)
-    .replace('<% css %>', page.css.code)
-    .replace('<% html %>', page.html)
+  // Return the page object to the SSR middleware so that it can be assembled
+  // into an HTML page and sent to the client.
+  return page
 }
