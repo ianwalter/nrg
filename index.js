853622c34a794c6c0cb452d14d1388224a7e9eb2
Ian Walter <public@iankwalter.com>
2019-12-06 16:53:17 -0500

    Restructuring middleware

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const createApp = require('./lib/app')
+
 const {
   GeneralError,
   HttpError,
@@ -7,40 +8,62 @@ const {
   NotFoundError,
   ValidationError
 } = require('./lib/errors')
+
 const {
   validateRegistration,
   createAccount,
   getAccount
 } = require('./lib/middleware/account')
+
 const { hashPassword } = require('./lib/middleware/password')
+
 const { generateToken, insertToken } = require('./lib/middleware/token')
+
 const {
   generateEmailVerificationEmail,
   generatePasswordResetEmail,
   sendEmail
 } = require('./lib/middleware/email')
+
 const {
   validateLogin,
-  authenticateLogin,
-  handleAuthentication
+  authenticateLogin
 } = require('./lib/middleware/session')
+
 const { serveStatic, serveWebpack } = require('./lib/middleware/client')
+
 const { serveSsr } = require('./lib/middleware/ssr')
 
 module.exports = {
-  // Creates the application:
+  // Create Application:
   createApp,
-  // Error classes:
+
+  /* Error classes: */
+
   GeneralError,
   HttpError,
   BadRequestError,
   UnauthorizedError,
   NotFoundError,
   ValidationError,
-  // Middleware:
+
+  /* Middleware: */
+
+  // General:
+  addToResponse,
+  addToSsr,
+
+  // Serving:
+  serveStatic,
+  serveWebpack,
+  serveSsr,
+
+  // Registration:
   validateRegistration,
   hashPassword,
   createAccount,
+  generateToken,
+  insertToken,
   generateEmailVerificationEmail,
   sendEmail,
   registration: [
@@ -50,32 +73,72 @@ module.exports = {
     generateToken,
     insertToken,
     generateEmailVerificationEmail,
-    sendEmail
+    sendEmail,
+    addToResponse
   ],
+
+  // Email Verification:
+  validateEmailVerification,
+  getAccountWithEmailTokens,
+  verifyEmail,
   emailVerfication: [
-    // validateEmailVerification,
-    // getAccountWithEmailTokens
+    validateEmailVerification,
+    getAccountWithEmailTokens,
+    verifyEmail,
+    addToResponse
   ],
+
+  // Login:
   validateLogin,
   getAccount,
   authenticateLogin,
-  handleAuthentication,
   login: [
     validateLogin,
     getAccount,
     authenticateLogin,
-    handleAuthentication
+    addToResponse
   ],
+
+  // Forgot Password:
+  validateForgotPassword,
   generatePasswordResetEmail,
-  passwordReset: [
-    // validatePasswordReset,
-    // getAccountWithPasswordTokens,
+  forgotPassword: [
+    validateForgotPassword,
     generateToken,
     insertToken,
     generatePasswordResetEmail,
-    sendEmail
+    sendEmail,
+    addToResponse
   ],
-  serveStatic,
-  serveWebpack,
-  serveSsr
+
+  // Reset Password:
+  validatePasswordReset,
+  getAccountWithPasswordTokens,
+  passwordReset: [
+    validatePasswordReset,
+    getAccountWithPasswordTokens,
+    resetPassword,
+    addToResponse
+  ],
+
+  // Show Account:
+  requireAuthorization,
+  account: [
+    requireAuthorization,
+    getAccount,
+    addToResponse
+  ],
+
+  // Account Update:
+  requireAuthorization,
+  validateAccountUpdate,
+  validatePasswordChange,
+  updateAccount,
+  changePassword,
+  accountUpdate: [
+    requireAuthorization,
+    validateAccountUpdate,
+    updateAccount,
+    addToResponse
+  ]
 }
