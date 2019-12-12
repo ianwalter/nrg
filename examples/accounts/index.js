8483ab73d6fe01430aa79b16f9fda7a141267b62
Ian Walter <public@iankwalter.com>
2019-12-11 23:03:00 -0500

    ✅ Improving log config for accounts example

--- examples/accounts/index.js
+++ examples/accounts/index.js
@@ -2,7 +2,11 @@ const path = require('path')
 const { createApp, login } = require('../..')
 
 const app = createApp({
-  log: { level: 'info' },
+  log: {
+    level: 'info',
+    prettyPrint: { stream: false },
+    prettifier: require('@ianwalter/pino-print')
+  },
   db: {
     connection: {
       database: 'nrg',
