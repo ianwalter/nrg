b4eb0b2941b9762100afd5712046abf5e64f3933
Ian Walter <public@iankwalter.com>
2019-12-19 23:31:32 -0500

    Working mq test

--- examples/mq.js
+++ examples/mq.js
@@ -15,20 +15,22 @@ const app = createApp({
 // Add a handler that publishes a message, receives an acknowledgement, and
 // sends a successful response.
 app.use(async (ctx, next) => {
-  await ctx.mq.test.publish({ greeting: 'Hello!' })
+  await ctx.mq.test.pub({ greeting: 'Hello!' })
   ctx.body = app.msg
+})
+
+app.afterSetup(() => {
+  // Set up a consumer to the "test" queue that just acknowledges that the
+  // message was received.
+  app.mq.test.sub(async msg => {
+    app.msg = { msg: msg.content, received: new Date() }
+    return msg.ack()
+  })
 })
 
 // Export the app if required, otherwise start the server.
 if (module.parent) {
   module.exports = app
 } else {
-  app.start().then(() => {
-    // Set up a consumer to the "test' queue that just acknowledges that the
-    // message was received.
-    app.mq.test.consume(async msg => {
-      app.msg = { msg: msg.content, received: new Date() }
-      msg.ack()
-    })
-  })
+  app.start()
 }
