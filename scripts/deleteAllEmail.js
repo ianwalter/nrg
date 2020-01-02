153e7673bbfc0f4633872a0a600d31f5a19fc9c6
Ian Walter <public@iankwalter.com>
2020-01-02 17:37:43 -0500

    Simplifying deleteAllEmail script

--- scripts/deleteAllEmail.js
+++ scripts/deleteAllEmail.js
@@ -1,5 +1,3 @@
 const { requester } = require('@ianwalter/requester')
 
-const run = async () => requester.delete('http://localhost:1080/email/all')
-
-run()
+module.exports = async () => requester.delete('http://localhost:1080/email/all')
