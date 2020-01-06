54be36eb3c868ae144b550899b08989a9ae34b25
Ian Walter <public@iankwalter.com>
2020-01-06 16:36:15 -0500

    Enabling CSRF in NODE_ENV=test

--- tests/logout.tests.js
+++ tests/logout.tests.js
@@ -7,8 +7,7 @@ const [julian] = accounts
 test('Logout when not logged in', async ({ expect }) => {
   const response = await app.test('/logout').delete()
   expect(response.status).toBe(200)
-  // TODO: enable CSRF logic in NOD_ENV=test
-  // expect(response.body.csrfToken).toBeDefined()
+  expect(response.body.csrfToken).toBeDefined()
 })
 
 test('Logout when logged in', async ({ expect }) => {
@@ -23,7 +22,7 @@ test('Logout when logged in', async ({ expect }) => {
   // Logout.
   response = await app.test('/logout', response).delete()
   expect(response.status).toBe(200)
-  // expect(response.body.csrfToken).toBeDefined()
+  expect(response.body.csrfToken).toBeDefined()
 
   // Verify account data cannot be retrieved.
   response = await app.test('/account', response).get()
