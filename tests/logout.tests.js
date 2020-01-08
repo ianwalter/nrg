59aa316317b69b3a69a8053e895d1483b7f2a966
Ian Walter <public@iankwalter.com>
2020-01-08 12:55:38 -0500

    Improving logout test

--- tests/logout.tests.js
+++ tests/logout.tests.js
@@ -21,10 +21,16 @@ test('Logout when logged in', async ({ expect }) => {
 
   // Logout.
   response = await app.test('/logout', response).delete()
+  const { csrfToken } = response.body
   expect(response.status).toBe(200)
   expect(response.body.csrfToken.length).toBeGreaterThan(0)
 
   // Verify account data cannot be retrieved.
   response = await app.test('/account', response).get()
   expect(response.status).toBe(401)
+
+  // Verify a user can login with the returned CSRF token.
+  response.request.header['csrf-token'] = csrfToken
+  response = await app.test('/login', response).post({ ...julian, password })
+  expect(response.status).toBe(201)
 })
