84061c687d17b1d482f5c1bc2532b2651ac80867
Ian Walter <public@iankwalter.com>
2020-01-18 15:00:14 -0500

    Updating test name

--- tests/mq.tests.js
+++ tests/mq.tests.js
@@ -1,7 +1,7 @@
 const { test } = require('@ianwalter/bff')
 const app = require('../examples/mq')
 
-test('mq', async ({ expect }) => {
+test('Message Queue', async ({ expect }) => {
   const response = await app.test('/').get()
   expect(response.status).toBe(200)
   expect(response.body.msg.greeting).toBe('Hello!')
