f40e59a1004d8e5174a20f5ce21960f2cbc6ce5f
Ian Walter <public@iankwalter.com>
2020-01-30 01:25:06 -0500

    Fix test hang

--- tests/ssr.tests.js
+++ tests/ssr.tests.js
@@ -7,15 +7,16 @@ const addData = (ctx, next) => {
   ctx.result = result
   return next()
 }
+const end = ctx => (ctx.status = 200)
 
 test('addToSsr', async ({ expect }) => {
   const assertion = ctx => expect(ctx.state.ssr.song).toBe(result.song)
-  app.get('/', addData, nrg.addToSsr, assertion)
+  app.get('/', addData, nrg.addToSsr, assertion, end)
   await app.test('/').get()
 })
 
 test('addToSsr with namespace', async ({ expect }) => {
   const assertion = ctx => expect(ctx.state.ssr.next.ok.song).toBe(result.song)
-  app.get('/namespace', addData, nrg.addToSsr('next.ok'), assertion)
+  app.get('/namespace', addData, nrg.addToSsr('next.ok'), assertion, end)
   await app.test('/namespace').get()
 })
