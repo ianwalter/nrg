e94cf8973eafb75660f2002d84673d978e1bd5b9
Ian Walter <public@iankwalter.com>
2020-01-30 09:05:38 -0500

    Trying to debug test hang in CI

--- tests/ssr.tests.js
+++ tests/ssr.tests.js
@@ -9,16 +9,22 @@ const addData = (ctx, next) => {
 }
 const end = ctx => (ctx.status = 200)
 
-test('addToSsr', async ({ expect }, done) => {
-  const assertion = ctx => expect(ctx.state.ssr.song).toBe(result.song)
-  app.get('/', addData, nrg.addToSsr, assertion, end)
-  await app.test('/').get()
-  done()
+test('addToSsr', async ({ expect }) => {
+  try {
+    const assertion = ctx => expect(ctx.state.ssr.song).toBe(result.song)
+    app.get('/', addData, nrg.addToSsr, assertion, end)
+    await app.test('/').get()
+  } catch (err) {
+    console.error(err)
+  }
 })
 
-test('addToSsr with namespace', async ({ expect }, done) => {
-  const assertion = ctx => expect(ctx.state.ssr.next.ok.song).toBe(result.song)
-  app.get('/namespace', addData, nrg.addToSsr('next.ok'), assertion, end)
-  await app.test('/namespace').get()
-  done()
+test('addToSsr with namespace', async ({ expect }) => {
+  try {
+    const assertion = c => expect(c.state.ssr.next.ok.song).toBe(result.song)
+    app.get('/namespace', addData, nrg.addToSsr('next.ok'), assertion, end)
+    await app.test('/namespace').get()
+  } catch (err) {
+    console.error(err)
+  }
 })
