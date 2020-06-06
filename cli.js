e1f971273cc31c16b389a4d4ce1fad07053a4686
Ian Walter <pub@ianwalter.dev>
2020-06-06 14:16:43 -0400

    Removing helpText from print config command

--- cli.js
+++ cli.js
@@ -5,6 +5,7 @@ const { promises: fs } = require('fs')
 const cli = require('@ianwalter/cli')
 const { Print } = require('@ianwalter/print')
 const cloneable = require('@ianwalter/cloneable')
+const without = require('@ianwalter/without')
 const healthcheck = require('./lib/commands/healthcheck')
 
 const { _: commands, packageJson, ...config } = cli({
@@ -103,7 +104,8 @@ async function run () {
     await healthcheck({ config, print }, app)
   } else if (commands[0] === 'print') {
     if (commands[1] === 'config') {
-      print.info('Application config:', cloneable(app.context.options))
+      const config = without(cloneable(app.context.options), 'helpText')
+      print.info('Application config:', config)
     } else {
       app.logger.fatal('Print what? Available: config')
       process.exit(1)
