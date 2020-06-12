9a3d9a5de3bee92e213234acb5d4b0ce8066b227
Ian Walter <pub@ianwalter.dev>
2020-06-11 21:29:20 -0400

    Removing unused seed

--- seeds/02_tokens.js
+++ seeds/02_tokens.js
@@ -6,20 +6,12 @@ const token = 'iJustC4n7!gnore'
 const salt = bcrypt.genSaltSync(12)
 const encryptedToken = bcrypt.hashSync(token, salt)
 const unverifiedUser = accounts.find(a => a.firstName === 'Unverified User')
-const adminUser = accounts.find(a => a.firstName === 'Admin User')
 const tokens = [
   {
     value: encryptedToken,
     type: 'email',
     accountId: unverifiedUser.id,
     email: unverifiedUser.email,
-    expiresAt: addDays(new Date(), 1).toISOString()
-  },
-  {
-    value: encryptedToken,
-    type: 'password',
-    accountId: adminUser.id,
-    email: adminUser.email,
     expiresAt: addDays(new Date(), 1).toISOString()
   }
 ]
