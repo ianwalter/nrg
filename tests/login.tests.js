404e4225b554775178624b6f870c4bfdfc0371b1
Ian Walter <pub@ianwalter.dev>
2020-06-12 21:55:18 -0400

    Fixing comparePasswords refactor

--- tests/login.tests.js
+++ tests/login.tests.js
@@ -5,37 +5,38 @@ const { accounts, password } = require('../seeds/01_accounts')
 const generalUser = accounts.find(a => a.firstName === 'General')
 const disabledUser = accounts.find(a => a.firstName === 'Disabled')
 
-test('Login validation', async t => {
-  // Email required.
-  let response = await app.test('/login').post({ password })
-  t.expect(response.status).toBe(400)
-  t.expect(response.body).toMatchSnapshot()
-
-  // Password required.
-  response = await app.test('/login').post({ email: generalUser.email })
-  t.expect(response.status).toBe(400)
-  t.expect(response.body).toMatchSnapshot()
-
-  // Invalid credentials.
-  const payload = { ...generalUser, password: 'thisIsNotTheRightPw' }
-  response = await app.test('/login').post(payload)
+test('Login -> No email', async t => {
+  const response = await app.test('/login').post({ password })
   t.expect(response.status).toBe(400)
   t.expect(response.body).toMatchSnapshot()
 })
 
-test('Login with valid credentials', async t => {
+test('Login -> No password', async t => {
+  const response = await app.test('/login').post({ email: generalUser.email })
+  t.expect(response.status).toBe(400)
+  t.expect(response.body).toMatchSnapshot()
+})
+
+test('Login -> Invalid credentials', async t => {
+  const payload = { ...generalUser, password: 'thisIsNotTheRightPw' }
+  const response = await app.test('/login').post(payload)
+  t.expect(response.status).toBe(400)
+  t.expect(response.body).toMatchSnapshot()
+})
+
+test('Login -> Valid credentials', async t => {
   const response = await app.test('/login').post({ ...generalUser, password })
   t.expect(response.status).toBe(201)
   t.expect(response.body).toMatchSnapshot()
 })
 
-test('Login with disabled user', async t => {
+test('Login -> Disabled user', async t => {
   const response = await app.test('/login').post({ ...disabledUser, password })
   t.expect(response.status).toBe(400)
   t.expect(response.body).toMatchSnapshot()
 })
 
-test('Login when already logged in', async t => {
+test('Login -> Already logged in', async t => {
   const credentials = { ...generalUser, password }
   let response = await app.test('/login').post(credentials)
   response = await app.test('/login', response).post(credentials)
