3f45d4fb15558f835a71f86a7694958095746399
Ian Walter <pub@ianwalter.dev>
2020-06-14 11:57:17 -0400

    Working on account tests and improving debug

--- tests/account.tests.js
+++ tests/account.tests.js
@@ -1,4 +1,5 @@
 const { test } = require('@ianwalter/bff')
+const { getTestEmail, extractEmailToken, Account } = require('..')
 const app = require('../examples/accounts')
 const { accounts, password } = require('../seeds/01_accounts')
 
@@ -95,9 +96,10 @@ test('Account -> Update password', async t => {
   t.expect(response.body).toMatchSnapshot()
 })
 
-test.skip('Account -> Update read-only data', async t => {
+test('Account -> Update read-only data', async t => {
   // Login.
   let response = await app.test('/login').post({ ...readOnlyUser, password })
+  const accountData = response.body
   t.expect(response.status).toBe(201)
 
   // Attempt to update emailVerified.
@@ -105,43 +107,74 @@ test.skip('Account -> Update read-only data', async t => {
   t.expect(response.status).toBe(200)
 
   // Verify that the value hasn't changed in the database.
+  const record = await Account.query().findById(readOnlyUser.id)
+  t.expect(response.body).toEqual(accountData)
+  t.expect(record).toMatchSnapshot({
+    createdAt: t.expect.any(Date),
+    updatedAt: t.expect.any(Date),
+    password: t.expect.any(String)
+  })
 
   // Attempt to update other read-only properties.
+  const data = { createdAt: new Date(), updatedAt: new Date(), enabled: false }
+  response = await app.test('/account', response).put(data)
+  t.expect(response.status).toBe(200)
 
   // Verify that none of the values have changed in the database.
+  t.expect(response.body).toEqual(accountData)
+  const updated = await Account.query().findById(readOnlyUser.id)
+  t.expect(record).toEqual(updated)
 })
 
-test.skip('Account -> Update email address', async t => {
+test('Account -> Update email address', async t => {
   // Login.
   let response = await app.test('/login').post({ ...changeEmailUser, password })
   t.expect(response.status).toBe(201)
-  const accountData = response.body
+  let accountData = response.body
 
   // Attempt to update email to an invalid email address.
+  let email = 'changed_email_test@example'
+  response = await app.test('/account', response).put({ email })
+  t.expect(response.status).toBe(400)
+  t.expect(response.body).toMatchSnapshot()
 
   // Attempt to update just the account's email address.
-  response = await app.test('/account', response).put({ })
+  email += '.com'
+  response = await app.test('/account', response).put({ email })
   t.expect(response.status).toBe(200)
 
   // Verify that account data hasn't changed in the response or the database.
   t.expect(response.body).toEqual(accountData)
 
   // Verify that the email verification email was received.
+  const byEmail = e => e.headers.to === email
+  await t.asleep(500)
+  t.expect(await getTestEmail(byEmail)).toBeDefined()
 
   // Attempt to update multiple account properties as well as the email address.
+  const data = { firstName: 'Changed Email', lastName: 'Testini' }
+  response = await app.test('/account', response).put({ email, ...data })
+  t.expect(response.status).toBe(200)
 
   // Verify that the account data except for the email address has been updated
   // in the response body and database.
-  t.expect(response.body).toEqual(accountData)
+  t.expect(response.body).toEqual({ ...accountData, ...data })
 
   // Verify the new email address.
+  await t.asleep(500)
+  const { token } = await extractEmailToken(byEmail)
+  response = await app.test('/verify-email').post({ email, token })
+  t.print.log(response.body, email, token)
+  t.expect(response.status).toBe(201)
+  accountData = response.body
 
   // Log out.
-  response = await app.test('/logout').delete()
+  response = await app.test('/logout', response).delete()
   t.expect(response.status).toBe(200)
 
   // Verify that the user can login with the new email address.
-  response = await app.test('/login').post({ password })
+  response = await app.test('/login').post({ email, password })
   t.expect(response.status).toBe(201)
+  t.expect(response.body).toEqual(accountData)
   t.expect(response.body).toMatchSnapshot()
 })
