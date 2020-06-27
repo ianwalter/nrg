a14918af95042a4abf6340e351951a4837196533
Ian Walter <pub@ianwalter.dev>
2020-06-27 00:07:49 -0400

    Fixing comment

--- tests/forgotPassword.tests.js
+++ tests/forgotPassword.tests.js
@@ -50,7 +50,7 @@ test('Forgot Password • Registered email', async t => {
     headers: t.expect.any(Object)
   })
 
-  // Verify that forgot password token was inserted into the database.
+  // Verify the Forgot Password token was inserted into the database.
   const record = await Token.query().findOne({
     email: generalUser.email,
     type: 'password'
