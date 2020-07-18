5aef1fa608a38d83e13f303481498f39d1fbf9a5
Ian Walter <pub@ianwalter.dev>
2020-07-17 22:15:14 -0400

    DO SOMETHING

---
+++ scripts/pgDebug.js
@@ -0,0 +1,5 @@
+// A script to configure Postgres to log all statements since GitHub Actions
+// doesn't support setting a command for service containers.
+module.exports = async function pgDebug (app) {
+  await app.db.raw("SELECT set_config('log_statement', 'all', true)")
+}
