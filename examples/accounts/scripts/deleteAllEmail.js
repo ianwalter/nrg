714fada1ff5f54c921aef4295b59db3c17f79902
Ian Walter <pub@ianwalter.dev>
2020-07-19 22:09:38 -0400

    Adding MAILDEV_HOST

--- examples/accounts/scripts/deleteAllEmail.js
+++ examples/accounts/scripts/deleteAllEmail.js
@@ -1,5 +1,5 @@
 const { requester } = require('@ianwalter/requester')
 
-const host = process.env.SMTP_HOST || 'localhost'
+const host = process.env.MAILDEV_HOST || 'localhost'
 const port = process.env.MAILDEV_PORT
 module.exports = async () => requester.delete(`http://${host}:${port}/email/all`)
--- packages/nrg/lib/utilities/getTestEmail.js
+++ packages/nrg/lib/utilities/getTestEmail.js
@@ -1,7 +1,10 @@
 const { requester } = require('@ianwalter/requester')
 
 module.exports = async function getTestEmail (byEmail, config = {}) {
-  const { host = 'localhost', port = process.env.MAILDEV_PORT } = config
+  const {
+    host = process.env.MAILDEV_HOST || 'localhost',
+    port = process.env.MAILDEV_PORT || 80
+  } = config
   const { body } = await requester.get(`http://${host}:${port}/email`)
   const email = body.find(byEmail)
   if (email) await requester.delete(`http://${host}:${port}/email/${email.id}`)
