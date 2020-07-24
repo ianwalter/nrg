4450ca6d183a928ea362004f67598b72c72786e1
Ian Walter <pub@ianwalter.dev>
2020-07-23 21:04:45 -0400

    Adding changeset

---
+++ .changeset/fluffy-pandas-move.md
@@ -0,0 +1,10 @@
+---
+"@ianwalter/nrg": minor
+"@ianwalter/nrg-mq": minor
+"@ianwalter/nrg-test": minor
+---
+
+- Changed app.start to app.serve
+- Refactored serve method and fixed a port config issue
+- Replaced supertest with nrg-test
+- Added the app.close method
--- packages/nrg-test/tests.js
+++ packages/nrg-test/tests.js
@@ -2,22 +2,26 @@ const { test } = require('@ianwalter/bff')
 const { createApp } = require('@ianwalter/nrg')
 const testApp = require('.')
 
-test('GET', async t => {
+test('get', async t => {
+  // Create the app instance.
   const app = createApp({ port: 80 }) // Port specified to test port override.
   const msg = 'Hooray!'
   app.use(ctx => (ctx.body = msg))
 
+  // Make a request to the app and verify the response is correct.
   const response = await testApp(app)('/').get()
   t.expect(response.statusCode).toBe(200)
   t.expect(response.body).toBe(msg)
 })
 
-test('POST', async t => {
+test('post', async t => {
+  // Create the app instance.
   const app = createApp()
   const payload = 'Hip!'
   const msg = 'Hooray!'
   app.post('/', ctx => (ctx.body = `${ctx.request.body.payload} ${msg}`))
 
+  // Make a request to the app and verify the response is correct.
   const response = await testApp(app)('/').post({ payload })
   t.expect(response.statusCode).toBe(200)
   t.expect(response.body).toBe(`${payload} ${msg}`)
