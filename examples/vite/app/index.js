c7eeee6b8579f73df796e156af2e698505adfbdc
Ian Walter <pub@ianwalter.dev>
2020-07-26 00:32:14 -0400

    Adding comment to vite example

--- examples/vite/app/index.js
+++ examples/vite/app/index.js
@@ -5,6 +5,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const app = createApp({
   port: 3000,
   plugins: {
+    // Disable compression in dev since vite already has compression built-in.
     ...!isProd ? { compress: false } : {}
   }
 })
