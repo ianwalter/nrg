920e31666613ff10701bc6302cd3169396b0cdfd
Ian Walter <pub@ianwalter.dev>
2020-07-29 00:21:21 -0400

    Fixing rate limit test

--- packages/nrg/tests/rateLimit.tests.js
+++ packages/nrg/tests/rateLimit.tests.js
@@ -4,6 +4,7 @@ const { createApp } = require('..')
 test('Rate limit', async t => {
   const app = createApp({
     rateLimit: {
+      client: 'memory',
       points: 1, // Allow one request...
       duration: 10 // every 10 seconds.
     }
