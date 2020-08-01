3cff5a5e00f20af1db88f06a446c4931d12b7b4c
Ian Walter <pub@ianwalter.dev>
2020-07-31 23:51:47 -0400

    Fixing copy migrations command

---
+++ .changeset/short-laws-call.md
@@ -0,0 +1,5 @@
+---
+"@ianwalter/nrg": patch
+---
+
+Fixing copy migrations command
--- packages/nrg/lib/commands/migrations.js
+++ packages/nrg/lib/commands/migrations.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const fs = require('fs')
+const { promises: fs } = require('fs')
 
 const migrationsSource = path.join(__dirname, '..', '..', 'migrations')
 
