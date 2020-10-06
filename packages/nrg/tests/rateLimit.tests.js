602bd8f544e687dfd55675e26dc4d85aa692c1b2
Ian Walter <pub@ianwalter.dev>
2020-10-06 00:11:14 -0400

    Update rateLimit.tests.js

--- packages/nrg/tests/rateLimit.tests.js
+++ packages/nrg/tests/rateLimit.tests.js
@@ -4,7 +4,6 @@ const { createApp } = require('..')
 test('Rate limit', async t => {
   const app = createApp({
     rateLimit: {
-      client: 'memory',
       points: 1, // Allow 1 request...
       duration: 10 // every 10 seconds.
     }
