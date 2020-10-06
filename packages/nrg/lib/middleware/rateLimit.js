61d77bb50e6110f1b75db2d948d7e41839b9372a
Ian Walter <pub@ianwalter.dev>
2020-10-05 23:46:57 -0400

    Trying ctx.request.ip

--- packages/nrg/lib/middleware/rateLimit.js
+++ packages/nrg/lib/middleware/rateLimit.js
@@ -1,10 +1,10 @@
 function rateLimit (rateLimiter) {
   return async (ctx, next) => {
     try {
-      await rateLimiter.consume(ctx.ip)
+      await rateLimiter.consume(ctx.request.ip)
       return next()
     } catch (err) {
-      ctx.logger.warn('Rate limit', ctx.ip, err)
+      ctx.logger.warn('Rate limit', ctx.request.ip, err)
       ctx.status = 429
       ctx.body = 'Too Many Requests'
     }
