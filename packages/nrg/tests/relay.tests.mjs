b9932722eb6d71c0456fbead37ebe1995520d70a
Ian Walter <pub@ianwalter.dev>
2021-02-24 11:23:04 -0500

    Adding additional relay test

--- packages/nrg/tests/relay.tests.mjs
+++ packages/nrg/tests/relay.tests.mjs
@@ -42,3 +42,30 @@ test('Relay • POST', async t => {
 
   await serverA.destroy()
 })
+
+test('Relay • Modified body', async t => {
+  const appA = nrg.createApp({ name: 'A', log: { level: 'info' } })
+  const appB = nrg.createApp({ name: 'B' })
+  const serverA = await appA.serve()
+
+  // Add the POST endpoint to app A.
+  appA.post('/', async ctx => (ctx.body = ctx.request.body))
+
+  // Create the relay middleware that will forward requests to app A.
+  const relay = nrg.relay({ baseUrl: serverA.url })
+
+  // Use the greet and relay middleware with app B.
+  const greet = (ctx, next) => {
+    ctx.state.relay = { body: { msg: `Hello ${ctx.request.body.name}!` } }
+    return next()
+  }
+  appB.post('/', greet, relay, nrg.addToResponse)
+
+  // Make the request and verify the response is correct.
+  const name = 'toots'
+  const response = await appB.test('/').post({ name })
+  t.expect(response.statusCode).toBe(200)
+  t.expect(response.body?.msg).toBe(`Hello ${name}!`)
+
+  await serverA.destroy()
+})
